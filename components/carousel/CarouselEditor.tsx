033090437125886e11593c8640409cd99c421133
Sarut <prince@Princes-MacBook-Pro.local>
2022-08-23 22:48:15 +0700

    fixed by comment 230822

--- components/carousel/CarouselEditor.tsx
+++ components/carousel/CarouselEditor.tsx
@@ -26,51 +26,56 @@ function CarouselEditor({
         Cover
       </div>
       <div className="bg-white p-5">
-      <Row justify="center" align="bottom" className="mt-10">
-
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={onFinish}
-        >
-          <Form.Item wrapperCol={{span: 12}} rules={[{ required: true }]} label="Title" name="title">
-            <Input placeholder="title" />
-          </Form.Item>
-          <Form.Item wrapperCol={{span: 12}}  label="LinkOut" name="linkOut">
-            <Input placeholder="Linkout" />
-          </Form.Item>
-          <Form.Item rules={[{ required: true }]} name="image">
-            <ImageUploader
-              width={1004}
-              height={360}
-              aspect={16/9}
-              setImage={setImage}
-              currentImageUrl={carouselData?.imageUrl || ""}
-            />
-          </Form.Item>
-          <div className="lead text-lg text-center">
-            Size (W x H) 1004 x 360 px
-            <br />
-            16 : 9
-          </div>
-
-          <Row justify="center" align="bottom" className="mt-10">
-            <Form.Item>
-              <Button
-                type="ghost"
-                onClick={() => router.push("/carousel")}
-                className="mr-5"
+        <Row justify="center" align="bottom" className="mt-10 w-full">
+          <Col span={22}>
+            <Form form={form} layout="vertical" onFinish={onFinish}>
+              <Form.Item
+                wrapperCol={{ span: 12 }}
+                rules={[{ required: true }]}
+                label="Title"
+                name="title"
               >
-                Back
-              </Button>
-              <Button type="primary" htmlType="submit">
-                {isEdit ? "Confirm" : "Create"}
-              </Button>
-            </Form.Item>
-          </Row>
-        </Form>
-      </Row>
+                <Input placeholder="title" />
+              </Form.Item>
+              <Form.Item
+                wrapperCol={{ span: 12 }}
+                label="LinkOut"
+                name="linkOut"
+              >
+                <Input placeholder="Linkout" />
+              </Form.Item>
+              <Form.Item rules={[{ required: true }]} name="image">
+                <ImageUploader
+                  width={1004}
+                  height={360}
+                  aspect={1004 / 360}
+                  setImage={setImage}
+                  currentImageUrl={carouselData?.imageUrl || ""}
+                />
+              </Form.Item>
+              <div className="lead text-lg text-center">
+                Size (W x H) 1004 x 360 px
+                <br />
+                16 : 9
+              </div>
 
+              <Row justify="center" align="bottom" className="mt-10">
+                <Form.Item>
+                  <Button
+                    type="ghost"
+                    onClick={() => router.push("/carousel")}
+                    className="mr-5"
+                  >
+                    Back
+                  </Button>
+                  <Button type="primary" htmlType="submit">
+                    {isEdit ? "Confirm" : "Create"}
+                  </Button>
+                </Form.Item>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
       </div>
     </div>
   );
